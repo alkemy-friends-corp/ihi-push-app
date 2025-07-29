195ae4fc7d4fda137d4a113f6c565523a95c8e0f
Csalkemy <alkemy@teamfriends.co.jp>
2025-07-29 09:54:07 +0600

    select common form

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { AuthGuardComponent } from './auth-guard';
 export * from './language-switcher';
 export * from './admin-layout';
 export * from './weather-widget';
+export * from './common-select';
